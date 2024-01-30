9ae1b9dcabbda975f5ab88f247bf14384211ff57
Ka1kizzz <156878870+Ka1kizzz@users.noreply.github.com>
2024-01-30 14:15:24 +0600

    Add files via upload

---
+++ Lab2/Dictionaries.py
@@ -0,0 +1,47 @@
+#ex1
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+#answer = print(car.get("model"))
+
+#ex2
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+#answer = car["year"] = 2020
+
+#ex3
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["color"] = "red"
+#answer = car["color"] = "red"
+
+#ex4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+#answer = car.pop("model")
+
+#ex5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+#answer = car.clear()
+
+
+
---
+++ Lab2/Sets.py
@@ -0,0 +1,30 @@
+#ex1
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#answer = "in"
+
+#ex2
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+#answer = fruits.add("orange")
+
+#ex3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+#answer = fruits.update(more_fruits)
+
+#ex4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+#answer = fruits.remove("banana")
+
+#ex5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+#answer = fruits.discard("banana")
+
+
+
+
